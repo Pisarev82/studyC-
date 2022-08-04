23f7aea3498e715977738e0c09dbcaefbbdfe906
Nikolay Pisarev <pisarev82@yandex.ru>
2022-08-04 13:37:34 +0300

    Новая папка и условие ветвления. Приведение строки к нижнему регистру

---
+++ 005_conditions_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine ();
+
+if(username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Ура, Это же МАША!");
+}
+else
+{
+    System.Console.Write("Привет, ");
+    System.Console.WriteLine(username);
+}
