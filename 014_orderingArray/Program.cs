f026e31b0883a2ca993ce05f6c49b2c6d357a71c
pisarev82 <pisarev82@yandex.ru>
2022-08-13 15:42:17 +0300

    Разбор задач из лекции. Виды фунций, но отработка только типа void. Замена замена всех нахождение символа, сортировка массива методом минимума.

---
+++ 014_orderingArray/Program.cs
@@ -0,0 +1,36 @@
+﻿// 
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray (int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void OrderingArray (int[] array)
+{
+    int length = array.Length;
+    
+    for (int i = 0; i < length - 1 ; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+Console.WriteLine();
+OrderingArray(arr);
+PrintArray(arr);
